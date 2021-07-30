4294f535b0fc06cd868064192ee0495f0e84cd89
gopinath <gopinathkaiser@gmail.com>
2021-07-30 17:09:58 +0530

    Delete products in firebase

--- src/App.js
+++ src/App.js
@@ -85,10 +85,17 @@ handleDecreaseQuantity = (product) =>{
 handlerDeleteProduct = (id) => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id);
-    this.setState({
-        products : items
+    const docRef = this.db.collection('products').doc(id);
+
+    docRef
+    .delete()
+    .then(() => {
+        console.log('Deleted Successfully')
     })
+    .catch((error)=>{
+        console.log('Error:', error);
+    })
+    
 }
 
 
